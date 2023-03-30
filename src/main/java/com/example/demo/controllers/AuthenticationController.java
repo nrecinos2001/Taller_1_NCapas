0f46a1f7e323c0424621088eecbc7ba2c3493eb2
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-03-29 23:30:41 -0600

    :sparkles: Update bussiness logic

--- src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
-import java.sql.Date;
+import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +21,11 @@ import com.example.demo.models.entities.User;
 public class AuthenticationController {
 	
 	private static List<User> users = new ArrayList<>();
-    
     static {
-        users.add(new User("AH000011", "pass", "Arturo", "Hernandez", "user", true, new Date(10102020)));
-        users.add(new User("AO000022", "pass", "Alejandro", "Ortega", "user", false, new Date(10102021)));
-        users.add(new User("JP000033", "pass", "Josue", "Perez", "user", true, new Date(15102022)));
-        users.add(new User("NR000044", "pass", "Nestor", "Recinos", "admin", true, new Date(10102019)));
+        users.add(new User("AH000011", "pass", "Arturo", "Hernandez", "user", true, new Date("10/10/2020")));
+        users.add(new User("AO000022", "pass", "Alejandro", "Ortega", "user", false, new Date("10/10/2021")));
+        users.add(new User("JP000033", "pass", "Josue", "Perez", "user", true, new Date("11/10/2024")));
+        users.add(new User("NR000044", "pass", "Nestor", "Recinos", "admin", true, new Date("10/12/2019")));
     }
     
 	@GetMapping("/login-web")
@@ -32,15 +33,33 @@ public class AuthenticationController {
 		return "login";
 	}
 	
+	@SuppressWarnings("deprecation")
 	@PostMapping("/login")
-	public String logIn(@ModelAttribute LoginDto loginDto, Model model) {
-		String pageToShow = "not-found";
+	public String logIn(@ModelAttribute LoginDto loginDto, Model model) {		
 		for (User user: users) {
 			if(user.getCode().equals(loginDto.getCode()) && user.getPassword().equals(loginDto.getPassword())) {
-				pageToShow = "dashboard";
-				model.addAttribute("users", users);
+				LocalDateTime actualDate = LocalDateTime.now();
+			    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+			    String formattedDate = actualDate.format(formatter);
+			     
+				if (user.getHired().after(new Date(formattedDate))) {
+					model.addAttribute("code", "403");
+					model.addAttribute("message", "El usuario todavia no ha iniciado labores");
+					return "not-found";
+				}
+				if (user.getRole().equals("admin")) {
+					model.addAttribute("admin", user);
+					model.addAttribute("users", users);
+					return "admin-dashboard";
+				} else {
+					model.addAttribute("user", user);
+					model.addAttribute("date", formattedDate);
+					return "user-dashboard";
+				}
 			}
-		}	
-		return pageToShow;
+		}
+		model.addAttribute("code", "404");
+		model.addAttribute("message", "El usuario no fue encontrado");
+		return "not-found";
 	}
 }
